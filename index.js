bccbfa6318550051de3f53666e8c4db4651514f2
aruns <arun1.s@ensono.com>
2024-12-30 14:10:16 +0530

    index modified

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/getuserdata2', function(req, res){
    res.send("Hello world!");
 });
 
-
+app.get('/modifyuser', function(req, res){
+   res.send("Hello world2!");
+});
 
 app.listen(29091);
