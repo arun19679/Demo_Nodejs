b65295657fef11abf80c5cc0a0904aa4f1d56058
Sree Ram <SreeRam.SS@ensono.com>
2024-12-30 14:08:39 +0530

    Changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/getuserdata', function(req, res){
+app.get('/getuserdata2', function(req, res){
    res.send("Hello world!");
 });
+
+
 
 app.listen(29091);
